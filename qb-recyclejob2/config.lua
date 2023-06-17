43b235692a5afd05dcca9c06504ebc0a956d9b88
shikohadou <89039934+shikohadou@users.noreply.github.com>
2023-06-17 06:51:43 +0100

    Add files via upload

---
+++ qb-recyclejob2/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config['delivery'] = {
+	outsideLocation = vector4(1066.39, -2388.78, 29.51, 172.54),
+	insideLocation = vector4(1105.0, -3099.2, -39.5, 269.32),
+	pickupLocations = {
+		[1] 	= vector4(1088.82, -3096.58, -37.0 -1.00, 178.42),
+		[2] 	= vector4(1091.31, -3096.15, -37.0 -1.00, 354.13),
+		[3] 	= vector4(1095.08, -3096.12, -37.0 -1.00, 355.7),
+		[4] 	= vector4(1097.64, -3096.06, -37.0 -1.00, 0.22),
+		[5] 	= vector4(1101.29, -3096.01, -37.0 -1.00, 356.86),
+		[6] 	= vector4(1103.93, -3096.06, -37.0 -1.00, 359.77),
+	},
+	dropLocation = vector4(1087.44, -3099.39, -39.0, 83.71),
+	warehouseObjects = {
+		"prop_boxpile_05a",
+		"prop_boxpile_04a",
+		"prop_boxpile_06b",
+		"prop_boxpile_02c",
+		"prop_boxpile_02b",
+		"prop_boxpile_01a",
+		"prop_boxpile_08a",
+	},
+}
